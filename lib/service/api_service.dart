6326e23a56779df74f0e2f653910c16788eb20f6
Your Name <you@example.com>
2024-01-21 11:12:47 +0700

    add dio

---
+++ lib/service/api_service.dart
@@ -0,0 +1,45 @@
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final dio = Dio(BaseOptions(baseUrl: "https://api.amiearth.com"));
+
+  dynamic get({required String path}) async {
+    Response res = await dio.get(path);
+    if (res.statusCode == 200) {
+      return res.data['data'];
+    }
+    return null;
+  }
+
+  dynamic post({
+    required String path,
+    required Map<String, dynamic> body,
+  }) async {
+    Response res = await dio.post(path,data: body);
+    if (res.statusCode == 200) {
+      return res.data['data'];
+    }
+    return null;
+  }
+
+  dynamic put({
+    required String path,
+    required Map<String, dynamic> body,
+  }) async {
+    Response res = await dio.put(path,data: body);
+    if (res.statusCode == 200) {
+      return res.data['data'];
+    }
+    return null;
+  }
+
+  dynamic delete({
+    required String path,
+  }) async {
+    Response res = await dio.delete(path);
+    if (res.statusCode == 200) {
+      return res.data['data'];
+    }
+    return null;
+  }
+}
