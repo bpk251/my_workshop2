00d138b1d08d3e6225c88ed7c7600dbd5dcd4f8d
Your Name <you@example.com>
2024-01-21 10:29:19 +0700

    123

--- lib/view/input_form_view.dart
+++ lib/view/input_form_view.dart
@@ -8,10 +8,92 @@ class InputFormView extends StatefulWidget {
 }
 
 class _InputFormViewState extends State<InputFormView> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController imageController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+  TextEditingController priceController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Input form"),
+      appBar: AppBar(
+        title: Text("Add Product"),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: nameController,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(45.0)),
+                    borderSide: BorderSide(color: Colors.orange),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(45.0)),
+                    borderSide: BorderSide(color: Colors.orange),
+                  ),
+                  hintText: "Product Name",
+                  labelText: "Product Name"),
+            ),
+            SizedBox(height: 16),
+            TextFormField(
+              controller: imageController,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(45.0)),
+                    borderSide: BorderSide(color: Colors.orange),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(45.0)),
+                    borderSide: BorderSide(color: Colors.orange),
+                  ),
+                  hintText: "Product Image",
+                  labelText: "Product Image"),
+            ),
+            SizedBox(height: 16),
+            TextFormField(
+              controller: descriptionController,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(45.0)),
+                    borderSide: BorderSide(color: Colors.orange),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(45.0)),
+                    borderSide: BorderSide(color: Colors.orange),
+                  ),
+                  hintText: "Product Description",
+                  labelText: "Product Description"),
+            ),
+            SizedBox(height: 16),
+            TextFormField(
+              controller: priceController,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(45.0)),
+                    borderSide: BorderSide(color: Colors.orange),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(45.0)),
+                    borderSide: BorderSide(color: Colors.orange),
+                  ),
+                  hintText: "Product Price",
+                  labelText: "Product Price"),
+            ),
+            SizedBox(height: 32),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text("Save"),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
